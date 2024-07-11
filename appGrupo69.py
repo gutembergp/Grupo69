9781cc740335b5a8469f5e7350ba52e878413f42
Gutemberg Almeida <39963984+gutembergp@users.noreply.github.com>
2024-07-11 11:55:00 -0300

    Update appGrupo69.py

--- appGrupo69.py
+++ appGrupo69.py
@@ -2,7 +2,6 @@
 import streamlit as st 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from utils import DropFeatures, OneHotEncodingNames, OrdinalFeature, MinMaxWithFeatNames
 from sklearn.pipeline import Pipeline
 import joblib
 from joblib import load
